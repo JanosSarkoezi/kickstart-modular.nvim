574847ae2bd2a59b325ddd324b01466ff5fe3eaf
Janos Sarkoezi <sarkoezi@intervista.at>
2024-05-28 14:43:36 +0200

    Drei Plugins hinzugefügt: blame, oil und vimtex

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,6 +5,26 @@
 
 return {
   {
+    'FabijanZulj/blame.nvim',
+    config = function()
+      require('blame').setup()
+    end,
+  },
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here
+    end,
+  },
+  {
     'christoomey/vim-tmux-navigator',
     config = function()
       local key_map = vim.api.nvim_set_keymap
